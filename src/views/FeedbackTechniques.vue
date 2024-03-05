6d0927222432e834decfc786d0b5dbb3a6f50758
Hui Mu <hui.mu@adelaide.edu.au>
2024-03-05 13:15:48 +1030

    remove dark backdrop for modal

--- src/views/FeedbackTechniques.vue
+++ src/views/FeedbackTechniques.vue
@@ -20,10 +20,10 @@
     </div>
   </div>
 
-  <div v-if="showModal !== null" class="fixed top-0 left-0 z-20 flex justify-center items-center w-screen h-screen bg-black/50 backdrop-blur-sm">
-    <div class="flex max-h-[90vh] w-11/12 max-w-2xl flex-col overflow-y-scroll rounded bg-white px-8 py-4 shadow-lg">
+  <div v-if="showModal !== null" class="fixed top-0 left-0 z-20 flex justify-center items-center w-screen h-screen backdrop-blur-sm">
+    <div class="flex max-h-[90vh] w-11/12 max-w-2xl flex-col overflow-y-scroll rounded-lg bg-white px-8 py-4 shadow-md drop-shadow-md">
       <div class="flex justify-between mb-2">
-        <h2 class="text-xl font-bold">{{ cardData[showModal].title }}</h2>
+        <div class="text-2xl font-bold my-auto">{{ cardData[showModal].title }}</div>
         <div class="flex justify-center items-center w-8 h-8 rounded-full bg-primary-tint text-gray-400 hover:bg-gray-300 hover:text-gray-700 cursor-pointer" @click="closeModal">
           <i class="far fa-times text-lg"/>
         </div>
