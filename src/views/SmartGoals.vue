99169558333a556ef777d54d9f9ba3640650fd94
Hui Mu <hui.mu@adelaide.edu.au>
2024-03-08 16:50:28 +1030

    minor style changes

--- src/views/SmartGoals.vue
+++ src/views/SmartGoals.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="w-full min-w-[760px] grid grid-cols-1 gap-3 items-center px-[2%] py-6">
     <div v-for="goal in goals" class="group h-30 w-full mx-auto">
-      <div class="relative w-full h-24 rounded-r-xl shadow-lg transition-all duration-500 border-l-4 [transform-style:preserve-3d] group-hover:[transform:rotateX(180deg)]" :style="{background: goal.color+'30', borderColor: goal.color}">
+      <div class="relative w-full h-24 rounded-r-xl shadow-lg transition-all duration-500 border-l-4 [transform-style:preserve-3d] group-hover:[transform:rotateX(180deg)]" :style="{background: goal.color+'20', borderColor: goal.color}">
         <div class="absolute inset-0 w-full h-full flex justify-between [transform:rotateX(0deg)] [backface-visibility:hidden]">
           <div class="w-fit -ml-1 my-auto" :style="{background: goal.color}">
             <span class="flex text-center justify-center items-center h-24 w-24 text-4xl font-bold text-white my-auto scale-150">{{ goal.init }}</span>
           </div>
           <div class="flex text-left my-auto grow">
             <div class="w-fit my-auto ml-3">
-              <div class="w-28 font-bold text-lg my-auto">{{ goal.title }}</div>
+              <div class="w-[108px] font-bold text-lg my-auto">{{ goal.title }}</div>
             </div>
-            <div class="grow leading-5">{{ goal.explanation }}</div>
+            <div class="grow leading-5 my-auto">{{ goal.explanation }}</div>
           </div>
           <div class="flex justify-center text-center my-auto w-fit">
             <i :class="`fad ${goal.icon} text-4xl scale-125 w-20 my-auto`" :style="{color: goal.color}" />
           </div>
         </div>
-        <div class="absolute inset-0 h-full w-full rounded-r-xl bg-black/20 [transform:rotateX(180deg)] [backface-visibility:hidden]">
+        <div class="absolute inset-0 h-full w-full rounded-r-xl bg-black/10 [transform:rotateX(180deg)] [backface-visibility:hidden]">
           <div class="flex justify-center w-full h-full relative">
             <div class="w-full pl-2 mx-auto my-auto">
               <div class="w-fit max-w-[705px] mx-auto">
@@ -27,9 +27,8 @@
               </div>
             </div>
 
-            <div class="absolute top-0 right-0 flex text-center items-center justify-center text-xl font-bold text-white h-12 w-12 rounded-xl my-auto" :style="{background: goal.color+'80'}">
-              <span class="my-auto">{{ goal.init }}</span>
-<!--              <i :class="`fad ${goal.icon} text-xl w-20 my-auto`" />-->
+            <div class="absolute top-0 right-0 flex text-center items-center justify-center text-xl font-bold text-white h-12 w-12 rounded-xl my-auto">
+              <i :class="`fad ${goal.icon} text-3xl my-auto`" :style="{color: goal.color}" />
             </div>
           </div>
         </div>
